5b4208b143961fd696e14483c2396945e819cc4f
Dhara <dsm952004@gmail.com>
2024-07-04 16:14:35 +0530

    connect to mysql db

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from gtts import gTTS
 import os
+import mysql.connector
 
 app = Flask(__name__)
+
+# Configure MySQL
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Dhara77@9504",
+    database="Texttoaudio"
+)
+cursor = db.cursor()
 
 # Ensure the output directory exists within the static folder
 output_dir = os.path.join(app.static_folder, 'output')
@@ -15,10 +25,10 @@ def index():
         text = request.form.get('text')
         language = request.form.get('language')
 
+        # Check if a file is uploaded
         if 'file' in request.files:
             file = request.files['file']
             if file.filename != '':
-                # Read the content of the file
                 text = file.read().decode('utf-8')
 
         # Generate speech if text is available
@@ -28,17 +38,20 @@ def index():
             filepath = os.path.join(output_dir, filename)
             gtts_object.save(filepath)
 
-            # Redirect to result page after processing
+            # Save to MySQL
+            cursor.execute("INSERT INTO text (text, language) VALUES (%s, %s)", (text, language))
+            db.commit()
+
+            # Redirect to result page
             return redirect(url_for('result'))
 
-    # Render initial form page
     return render_template('index.html')
 
 @app.route('/result')
 def result():
     # Render the result page where you display the generated audio or other results
-    audio_url = url_for('static', filename=f'output/output.mp3')
+    audio_url = url_for('static', filename='output/output.mp3')
     return render_template('result.html', audio_url=audio_url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
